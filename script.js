26210a8b1abf21e321f537593188525ce4579078
Oscar1999-L <oscar.ramirez990719@gmail.com>
2024-09-18 23:53:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,6 @@ function iniciarCuentaRegresiva(fechaObjetivo) {
     const formatoMinutos = minutos.toString().padStart(2, '0');
     const formatoSegundos = segundos.toString().padStart(2, '0');
 
-    contador.textContent = `Faltan:`;
     contador.textContent = `${formatoDias} d. ${formatoHoras} h. ${formatoMinutos} m. ${formatoSegundos} s.`;
   }
 
